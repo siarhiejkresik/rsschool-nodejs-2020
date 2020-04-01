cd4955f5b5820a97c2f17f76a788ffe8e5c18529
Siarhiej Kresik <siarhiej.kresik@gmail.com>
2020-04-01 21:37:07 +0300

    fix: incorrect circular add

--- caesar-cipher-cli/utils.js
+++ caesar-cipher-cli/utils.js
@@ -1,10 +1,10 @@
 function circularAdd(current, value, start, size) {
   if (value > 0) {
-    return start + ((current - start + value) % size);
+    return start + ((current - start + (value % size)) % size);
   }
 
   if (value < 0) {
-    return start + ((current - start + size - value) % size);
+    return start + ((current - start + size + (value % size)) % size);
   }
 
   return current;
